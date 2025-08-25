ac2c93517b6660bd1cc82fae446bde8e29130420
Samuel Palacios <samuelitopr77@gmail.com>
2025-08-25 17:56:42 -0500

    En este comit, se avanzó hasta el punto 2
    ""

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,23 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.page: int = page
+        self.date: datetime = date
+
+    def __str__(self) -> str:
+        return f"{self.date} - page {self.page}: {self.text}"
+class Book:
+
+    EXELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, title: str, author: str, page: int):
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.page: int = page
+        self.rating: int = Book.UNRATED
