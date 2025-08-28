76b7b5976669ebc7caa0d02edba7ef042ddbef0b
samuel palacios <samuelitopr77@gmail.com>
2025-08-28 17:31:36 -0500

    Anvance clase Jueves 28 Agosto

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -15,9 +15,64 @@ class Book:
     BAD: int = 1
     UNRATED: int = -1
 
-    def __init__(self, isbn: str, title: str, author: str, page: int):
+    def __init__(self, isbn: str, title: str, author: str, pages: int):
         self.isbn: str = isbn
         self.title: str = title
         self.author: str = author
-        self.page: int = page
+        self.pages: int = pages
         self.rating: int = Book.UNRATED
+        self.notes: list[Note] = []
+
+    def add_note(self, text: str, page: int, date: datetime) -> bool:
+        if page > self.pages:
+            return False
+        note = Note(text, page, date)
+        self.notes.append(note)
+        return True
+
+    def set_rating(self, rating: int) -> bool:
+        if rating not in [Book.GOOD, Book.BAD, Book.EXELLENT]:
+            return False
+        self.rating = rating
+        return True
+
+    def get_note_of_page(self, page: int) ->list[Note]:
+        pass
+
+    def page_with_most_notes(self) -> int:
+        notes_counter: dict[int, int] = {}
+        for note in self.notes:
+            if note.page not in notes_counter.keys():
+                notes_counter[note.page] = 1
+            else:
+                notes_counter[note.page] += 1
+
+        max_page, max_counter = -1, 0
+        for page, count in notes_counter.items():
+            if count > max_counter:
+                max_page, max_counter = page, count
+        return max_page
+
+    def __str__(self) -> str:
+        str_rating: dict[int, str] = {Book.GOOD: "good", Book.BAD: "bad", Book.EXELLENT: "exelent", Book.UNRATED: "unrated"}
+        return f"ISBN: {self.isbn}\nTitle: {self.title}\nAutor: {self.author}\nPages {self.pages}\nRating: {str_rating[self.rating]}"
+
+class ReadingDiary:
+
+    def __init__(self):
+            self.books: dict[str, Book] = {}
+
+    def add_book(self, isbn: str, title: str, author: str, pages: int):
+        if isbn not in self.books:
+            return False
+        self.books[isbn] = Book(isbn, title, author, pages)
+        return True
+
+    def search_by_isbn(self, isbn: str) -> Book | None:
+        return self.books.get(isbn, None)
+
+       # def search_by_isbn(self, isbn: str) -> Book | None:
+        #   pass
+
+       # def add_note_to_book(self):
+        #   pass
